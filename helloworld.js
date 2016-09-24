3555e751645cc8192f819eb5b4c0219bcd03020c
Jun <shukatu.2012.5.25@gmail.com>
2016-09-24 15:27:55 +0900

    first commit.

---
+++ helloworld.js
@@ -0,0 +1,14 @@
+var Application = (function () {
+    function Application() {
+    }
+    Application.main = function () {
+        console.log('Hello world');
+        console.log('Hello world');
+        console.log('Hello world');
+        console.log('Hello world');
+        console.log('Hello world');
+    };
+    return Application;
+}());
+Application.main();
+//# sourceMappingURL=helloworld.js.map
